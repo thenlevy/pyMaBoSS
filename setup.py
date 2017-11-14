b19cd3fad521b3d0fddd161721bc17e629e83776
thenlevy <nicolaspierrelevy@gmail.com>
2017-11-14 18:19:15 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+
+
+from setuptools import setup, find_packages
+import maboss
+
+setup(name='maboss', version=0.1, packages=find_packages(), author="Nicolas Levy",
+      author_email="nicolaspierrelevy@gmail.com",
+      description="A python and jupyter api for the MaBoSS software")
