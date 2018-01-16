90f2095c158bd51635d25363edf4b8cc7d7b14be
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2018-01-16 13:31:50 +0100

    turn matplotlib interactivity on if within jupyter

--- maboss/__init__.py
+++ maboss/__init__.py
@@ -9,6 +9,9 @@ if IN_IPYTHON:
     from colomoto_jupyter import jupyter_setup
     from .widgets import *
 
+    import matplotlib.pyplot as plt
+    plt.ion()
+
     jupyter_setup("mymodule",
         label="MaBoSS",
         color="green", # for menu and toolbar
