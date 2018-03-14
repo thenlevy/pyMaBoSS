b770bf8df306d907bbb565aa8e97737a83d1eb78
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2018-03-14 16:32:10 +0100

    better legend placement for trajectory plots

--- maboss/figures.py
+++ maboss/figures.py
@@ -21,14 +21,17 @@ def make_plot_trajectory(time_table, ax, palette, legend=True):
     color_list = [persistent_color(palette, idx) \
                     for idx in time_table.columns.values]
     time_table.plot(ax=ax, color=color_list, legend=legend)
+    if legend:
+        plt.legend(loc='upper right')
 
 
 def plot_node_prob(time_table, ax, palette):
     """Plot the probability of each node being up over time."""
-    register_states_for_color(palette, time_table.index.values)
+    register_states_for_color(palette, time_table.columns.values)
     color_list = [persistent_color(palette, idx) \
-                    for idx in time_table.index.values]
+                    for idx in time_table.columns.values]
     time_table.plot(ax=ax, color=color_list)
+    plt.legend(loc='upper right')
 
 
 def plot_piechart(plot_table, ax, palette, embed_labels=True, autopct=False):
